2b3f628ea9e202bb736d75c49a3ffa50db93729d
Xiao (Mark) Chen <mark.chen.jq@gmail.com>
2019-12-21 00:08:23 -0800

    modification

--- read_data.py
+++ read_data.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-with pd.HDFStore('data/eg_realpower_1hr.h5') as hdf:
+
+dataname = 'data/eg_angle_15min.h5'
+with pd.HDFStore(dataname) as hdf:
     # This prints a list of all group names:
     print(hdf.keys())
 
-df=pd.read_hdf('data/eg_realpower_1hr.h5', key='/building1/elec/meter1')
+df=pd.read_hdf(dataname, key='/building1/elec/meter1')
 print(df)
